6cb862b3394468130abe3aaf89c2bcbbc55f1682
ahsantariq792 <tariqahsan63@gmail.com>
2021-10-08 04:56:43 +0500

    proc

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const port = 3000
-const app = express()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -23,6 +23,6 @@ app.delete('/profile', (req, res) => {
 })
 
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
